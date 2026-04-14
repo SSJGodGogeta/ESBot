242a8d55d3d5b87f9759390e6aaaf2e71a929673
Arman <130306534+SSJGodGogeta@users.noreply.github.com>
2026-04-14 20:44:49 +0200

    feat: Logging middleware

--- ESBot.API/Middleware/RequestLoggingMiddleware.cs
+++ ESBot.API/Middleware/RequestLoggingMiddleware.cs
@@ -17,22 +17,37 @@ public class RequestLoggingMiddleware
     public async Task Invoke(HttpContext context)
     {
         var stopwatch = Stopwatch.StartNew();
-
         var request = context.Request;
-
         var ip = context.Connection.RemoteIpAddress?.ToString();
         var method = request.Method;
         var path = request.Path;
-
         var userAgent = request.Headers.UserAgent.ToString();
-
         var userId =
             context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value
             ?? "anonymous";
+        var query = request.QueryString.ToString();
+        var correlationId = context.TraceIdentifier;
 
         try
         {
             await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                ex,
+                "Unhandled exception during request. " +
+                "CorrelationId={CorrelationId} | HTTP Method={Method} | Path={Path} | Query={Query} | IP={IP} | User={UserId} | UserAgent={UserAgent}",
+                correlationId,
+                method,
+                path,
+                query,
+                ip,
+                userId,
+                userAgent
+            );
+
+            throw;
         }
         finally
         {

0ade6b1286a3f7d07809a3b742da98e9c3ab7c00
11elfone <konrad_john@t-online.de>
2026-06-09 15:27:09 +0200

    testing CI on failed test

---
+++ ESBot.Tests/TestingTest.cs
@@ -0,0 +1,10 @@
+﻿namespace ESBot.Tests;
+
+public class TestingTest
+{
+    [Fact]
+    public void FailingTest_ForTesting()
+    {
+        Assert.Fail("This test should fail (testing CI)");
+    }
+}
